213e167a8df0ac658ae8bafb9ec0ced92c6bdc60
Denis Isidoro <denis.isidoro@uber.com>
2021-03-03 09:42:19 -0300

    Updates

---
+++ precache-manifest.a052f851c3239a455065dbbea89ec0a6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e0f06ad987f83df9199",
+    "url": "/pipers/168dd62f42d33cd0ca68.module.wasm"
+  },
+  {
+    "revision": "079fbe5aaf3628b275e271c4c4ec8fe0",
+    "url": "/pipers/index.html"
+  },
+  {
+    "revision": "fdbd34ff61d0c4b2c2f4",
+    "url": "/pipers/static/css/main.64bde5dd.chunk.css"
+  },
+  {
+    "revision": "7165de835baf2dbe7f9b",
+    "url": "/pipers/static/js/2.eb6b5ab1.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/pipers/static/js/2.eb6b5ab1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e0f06ad987f83df9199",
+    "url": "/pipers/static/js/3.33ee410f.chunk.js"
+  },
+  {
+    "revision": "fdbd34ff61d0c4b2c2f4",
+    "url": "/pipers/static/js/main.34c321ee.chunk.js"
+  },
+  {
+    "revision": "16270adee2cb0097e5b2",
+    "url": "/pipers/static/js/runtime-main.58222128.js"
+  }
+]);
